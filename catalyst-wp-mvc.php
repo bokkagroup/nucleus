4182f615d2dd391a87b3089df56b17d53c6b23bf
Charlie Stanard <charlie@bokkagroup.com>
2017-10-16 17:57:34 -0600

    Fixing spaces

--- catalyst-wp-mvc.php
+++ catalyst-wp-mvc.php
@@ -16,28 +16,28 @@ namespace CatalystWP;
  */
 class MVC {
 
-	private static $instance;
+    private static $instance;
 
-	public function __construct()
+    public function __construct()
     {
-		//Set directory variables
+        //Set directory variables
         define("CATALYST_WP_MVC_DIRECTORY",   plugin_dir_path(__FILE__));
         define("THEME_PARENT_DIR", get_template_directory());
         define("THEME_CHILD_DIR", get_stylesheet_directory());
 
         $this->loadHandlebars();
 
-		//load base classes
-		require_once(CATALYST_WP_MVC_DIRECTORY . 'controllers/BaseController.php');
-		require_once(CATALYST_WP_MVC_DIRECTORY . 'views/BaseView.php');
+        //load base classes
+        require_once(CATALYST_WP_MVC_DIRECTORY . 'controllers/BaseController.php');
+        require_once(CATALYST_WP_MVC_DIRECTORY . 'views/BaseView.php');
         require_once(CATALYST_WP_MVC_DIRECTORY . 'models/BaseModel.php');
-		require_once(CATALYST_WP_MVC_DIRECTORY . 'models/Menu.php');
+        require_once(CATALYST_WP_MVC_DIRECTORY . 'models/Menu.php');
 
         require_once(CATALYST_WP_MVC_DIRECTORY . 'autoloader.php');
 
-		//auto load controllers
+        //auto load controllers
         add_action('init', array($this, 'autoLoad'));
-	}
+    }
 
     private function loadHandlebars()
     {
@@ -68,11 +68,11 @@ class MVC {
 
     }
 
-	/**
-	 * Singleton instantiation
-	 * @return [static] instance
-	 */
-	public static function get_instance()
+    /**
+     * Singleton instantiation
+     * @return [static] instance
+     */
+    public static function get_instance()
     {
         if (null === static::$instance) {
             static::$instance = new static();
@@ -82,93 +82,93 @@ class MVC {
     }
 
 
-	public function autoLoad()
+    public function autoLoad()
     {
         self::loadFile('config.php');
         self::loadFiles( 'helpers' );
         new \CatalystWP\MVC\autoloader();
 
-		return;
-	}
+        return;
+    }
 
-	/**
-	 * Loads multiple files from theme directories, child theme overrides parent
-	 * @param  [string] $type [ a MVC file type i.e. "controllers", "models", "templates" ]
-	 * @return [void]
-	 */
-	public static function loadFiles($type)
+    /**
+     * Loads multiple files from theme directories, child theme overrides parent
+     * @param  [string] $type [ a MVC file type i.e. "controllers", "models", "templates" ]
+     * @return [void]
+     */
+    public static function loadFiles($type)
     {
-		$typeURI = strtolower('/' . $type . '/');
+        $typeURI = strtolower('/' . $type . '/');
         $parentFiles = array();
         $childFiles = array();
 
-		//make sure there are directories to introspect
-		if (!file_exists( THEME_CHILD_DIR . $typeURI)
-			&& !file_exists(THEME_PARENT_DIR . $typeURI)) {
-			error_log( 'Catalyst WP Error: '. __( 'Could not find any directories for {'. $type . '}, you may need to add a folder in your child or parent theme.', 'CATALYST_WP_MVC' ) );
-			return;
-		}
+        //make sure there are directories to introspect
+        if (!file_exists( THEME_CHILD_DIR . $typeURI)
+            && !file_exists(THEME_PARENT_DIR . $typeURI)) {
+            error_log( 'Catalyst WP Error: '. __( 'Could not find any directories for {'. $type . '}, you may need to add a folder in your child or parent theme.', 'CATALYST_WP_MVC' ) );
+            return;
+        }
 
-		//get some arrays of our filenames
-		if (file_exists(THEME_PARENT_DIR . $typeURI))
-			$parentFiles = array_diff(scandir(THEME_PARENT_DIR . $typeURI), array('.', '..') );
-
-
-		if (file_exists(THEME_CHILD_DIR . $typeURI))
-			$childFiles =  array_diff( scandir( THEME_CHILD_DIR . $typeURI ), array('.', '..') );
+        //get some arrays of our filenames
+        if (file_exists(THEME_PARENT_DIR . $typeURI))
+            $parentFiles = array_diff(scandir(THEME_PARENT_DIR . $typeURI), array('.', '..') );
 
 
-		//get the diff and remove dupes from parent (child overrides parent)
-		if (isset($parentFiles) && isset($childFiles))
-			$parentFiles = array_diff($parentFiles, $childFiles);
-
-		//load remaining parent files
-		if (isset($parentFiles)) {
-			foreach ($parentFiles as $file) {
-				require_once( THEME_PARENT_DIR . $typeURI . $file);
-			}
-		}
+        if (file_exists(THEME_CHILD_DIR . $typeURI))
+            $childFiles =  array_diff( scandir( THEME_CHILD_DIR . $typeURI ), array('.', '..') );
 
 
-		//load remaining child files
-		if (isset($childFiles) && $childFiles !== $parentFiles) {
-			foreach ($childFiles as $file) {
-				require_once(THEME_CHILD_DIR . $typeURI . $file);
-			}
-		}
+        //get the diff and remove dupes from parent (child overrides parent)
+        if (isset($parentFiles) && isset($childFiles))
+            $parentFiles = array_diff($parentFiles, $childFiles);
 
-		return;
+        //load remaining parent files
+        if (isset($parentFiles)) {
+            foreach ($parentFiles as $file) {
+                require_once( THEME_PARENT_DIR . $typeURI . $file);
+            }
+        }
 
-	}
 
-	/**
-	 * Loads a single file, child theme overrides parent
-	 * @param  [string] $fileName [full name of the file you are trying to include w/ extension]
-	 * @param  [sting] $type     [a MVC file type i.e. "controllers", "models", "templates"]
-	 * @return [void]           [description]
-	 */
-	public static function loadFile($fileName, $type = "")
+        //load remaining child files
+        if (isset($childFiles) && $childFiles !== $parentFiles) {
+            foreach ($childFiles as $file) {
+                require_once(THEME_CHILD_DIR . $typeURI . $file);
+            }
+        }
+
+        return;
+
+    }
+
+    /**
+     * Loads a single file, child theme overrides parent
+     * @param  [string] $fileName [full name of the file you are trying to include w/ extension]
+     * @param  [sting] $type     [a MVC file type i.e. "controllers", "models", "templates"]
+     * @return [void]           [description]
+     */
+    public static function loadFile($fileName, $type = "")
     {
-		$typeURI = $type ? strtolower('/' . $type . '/') : '/';
-		$childFileURI = THEME_CHILD_DIR . $typeURI . $fileName;
-		$parentFileURI = THEME_PARENT_DIR . $typeURI . $fileName;
+        $typeURI = $type ? strtolower('/' . $type . '/') : '/';
+        $childFileURI = THEME_CHILD_DIR . $typeURI . $fileName;
+        $parentFileURI = THEME_PARENT_DIR . $typeURI . $fileName;
 
-		//make sure the file exists
-		if(!file_exists($childFileURI) && !file_exists($parentFileURI)) {
-			error_log( 'Catalyst WP Warning: '. __( 'Could not find file {' . $typeURI . $fileName . '}, please create file.', 'CATALYST_WP_MVC' ) );
-			return;
-		}
+        //make sure the file exists
+        if(!file_exists($childFileURI) && !file_exists($parentFileURI)) {
+            error_log( 'Catalyst WP Warning: '. __( 'Could not find file {' . $typeURI . $fileName . '}, please create file.', 'CATALYST_WP_MVC' ) );
+            return;
+        }
 
-		//load it ( check child theme first )
-		if(file_exists($childFileURI))
-			require_once($childFileURI);
+        //load it ( check child theme first )
+        if(file_exists($childFileURI))
+            require_once($childFileURI);
 
-		if(file_exists($parentFileURI) && !file_exists($childFileURI))
-			require_once($parentFileURI);
+        if(file_exists($parentFileURI) && !file_exists($childFileURI))
+            require_once($parentFileURI);
 
-		return;
+        return;
 
-	}
+    }
 
 }
 
--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -6,49 +6,49 @@ global $BGMVC;
 
 Class Controller {
 
-	/**
-	 * Loads and instantiates model
-	 * @return [type] [description]
-	 */
-	public function loadModel(){
-		global $BGMVC;
-		//parse model class name
+    /**
+     * Loads and instantiates model
+     * @return [type] [description]
+     */
+    public function loadModel(){
+        global $BGMVC;
+        //parse model class name
         $controllerName = basename(str_replace('\\', '/', get_class($this)));
         $modelClass = explode("Controller",  $controllerName )[0];
 
-		//load the model file
-		\CatalystWP\MVC::loadFile( $modelClass . '.php', 'models' );
+        //load the model file
+        \CatalystWP\MVC::loadFile( $modelClass . '.php', 'models' );
         $modelClass = '\CatalystWP\theme\models\\'.$modelClass;
-		//instantiate the model
+        //instantiate the model
 
-		if (class_exists($modelClass))
+        if (class_exists($modelClass))
             return new $modelClass();
 
 
-	}
+    }
 
-	public function loadView( ){
-		global $BGMVC;
+    public function loadView( ){
+        global $BGMVC;
 
-		//parse view class name
+        //parse view class name
         $controllerName = basename(str_replace('\\', '/', get_class($this)));
-		$viewClass = explode("Controller",  $controllerName )[0] . 'View';
+        $viewClass = explode("Controller",  $controllerName )[0] . 'View';
 
-		//load the view file
+        //load the view file
         \CatalystWP\MVC::loadFile( $viewClass . '.php', 'views' );
         $viewClass = '\CatalystWP\theme\views\\'.$viewClass;
-		//instantiate the view
-		if( class_exists( $viewClass ) )
+        //instantiate the view
+        if( class_exists( $viewClass ) )
             return new $viewClass();
 
         return 0;
-	}
+    }
 
 
-	public function __construct(){
+    public function __construct(){
         $this->model = $this->loadModel();
         $this->view = $this->loadView();
 
-		$this->initialize();
-	}
+        $this->initialize();
+    }
 }
