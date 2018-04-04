bf60f2c65ba9ccad5b18bea02acd68a6799c8952
Charlie Stanard <charlie@bokkagroup.com>
2018-04-04 15:36:48 -0600

    Updating BaseModel to set data received from controller, adding methods to filter unneeded post properties and re-assign properties from data array to model itself

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -6,7 +6,39 @@ Class Model
 {
     public $data = array();
 
-    public function __construct($options = array())
+    /**
+     * Filter out properties of WP_Post not in $allowed
+     * @param  array  $allowed Array of allowed properties for model
+     * @return array           Filtered results
+     */
+    public function filterProperites($allowed = array(), $data = array())
+    {
+        // convert WP_Post object to array
+        $data = (array) $data;
+
+        // remove any non-allowed properties
+        $data = array_filter($data, function ($key) use ($allowed) {
+            return in_array($key, $allowed);
+        }, ARRAY_FILTER_USE_KEY);
+
+        return $data;
+    }
+
+    /**
+     * Take properties from $data and assign to primary model instance
+     * @param  array  $data Data to attach to instance
+     * @return void
+     */
+    public function createProperties($data = array())
+    {
+        foreach ($data as $key => $value) {
+            $this->{$key} = $value;
+        }
+
+        return;
+    }
+
+    public function __construct($options = array(), $data = array())
     {
         $this->options = $options;
 
@@ -14,7 +46,16 @@ Class Model
             switch_to_blog($this->options['parent_blog']);
         }
 
-        $this->initialize();
+        if (($data) && (property_exists($this, 'allowed') && $this::$allowed)) {
+            $data = $this->filterProperites($this::$allowed, $data);
+        }
+
+        if ($data) {
+            $this->createProperties($data);
+            unset($this->data);
+        }
+
+        $this->initialize($data);
 
         if (isset($this->options['parent_blog'])) {
             restore_current_blog();
