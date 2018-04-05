94e96ea2286f92beef2ee3a39a1af0f160bb0a1e
Charlie Stanard <charlie@bokkagroup.com>
2018-04-05 16:58:38 -0600

    Finishing Resource class

--- Resource.php
+++ Resource.php
@@ -2,45 +2,33 @@
 
 namespace CatalystWP\Nucleus;
 
+require_once(CATALYST_WP_NUCLEUS_DIRECTORY . 'helpers/Inflector.php');
+
+use Cake\Utility\Inflector as Inflector;
+
 Class Resource
 {
     public static function registerPostType($model, $resource = array())
     {
-        //TODO: Have default options w/ability to override
-
-        $defaultLabels = array();
-
-        $defaultArgs = array(
-            'label'                 => __( 'Post Type', 'text_domain' ),
-            'description'           => __( 'Post Type Description', 'text_domain' ),
-            'labels'                => $resource['labels'],
-            'supports'              => array('title', 'editor'),
-            'taxonomies'            => array(),
-            'hierarchical'          => false,
-            'public'                => true,
-            'show_ui'               => true,
-            'show_in_menu'          => true,
-            'menu_position'         => 5,
-            'show_in_admin_bar'     => true,
-            'show_in_nav_menus'     => true,
-            'can_export'            => true,
-            'has_archive'           => true,
-            'exclude_from_search'   => false,
-            'publicly_queryable'    => true,
-            'capability_type'       => 'page',
-        );
-
         $slug = getModelSlug($model);
 
         if (post_type_exists($slug)) {
             return;
         }
 
+        $namespace = explode('\\', $model);
+        $modelName = end($namespace);
+
+        $plural = Inflector::pluralize($modelName);
+
         $labels = array(
-            'name'                  => _x( 'Post Types', 'Post Type General Name', 'text_domain' ),
-            'singular_name'         => _x( 'Post Type', 'Post Type Singular Name', 'text_domain' ),
-            'menu_name'             => __( 'Post Types', 'text_domain' ),
-            'name_admin_bar'        => __( 'Post Type', 'text_domain' ),
+            // User-defined or auto-generated
+            'name'                  => _x( "${plural}", 'Post Type General Name', 'text_domain' ),
+            'singular_name'         => _x( "${modelName}", 'Post Type Singular Name', 'text_domain' ),
+            'menu_name'             => __( "${plural}", 'text_domain' ),
+            'name_admin_bar'        => __( "${modelName}", 'text_domain' ),
+
+            // Generic labels
             'archives'              => __( 'Item Archives', 'text_domain' ),
             'attributes'            => __( 'Item Attributes', 'text_domain' ),
             'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
@@ -60,11 +48,15 @@ Class Resource
             'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
         );
 
+        if (isset($resource['labels'])) {
+            $labels = array_merge($labels, $resource['labels']);
+        }
+
         $args = array(
             'label'                 => __( 'Post Type', 'text_domain' ),
             'description'           => __( 'Post Type Description', 'text_domain' ),
-            'labels'                => $resource['labels'],
-            'supports'              => array('title'),
+            'labels'                => array(),
+            'supports'              => array('title', 'editor'),
             'taxonomies'            => array(),
             'hierarchical'          => false,
             'public'                => true,
@@ -74,11 +66,17 @@ Class Resource
             'show_in_admin_bar'     => true,
             'show_in_nav_menus'     => true,
             'can_export'            => true,
-            'has_archive'           => $resource['options']['has_archive'],
+            'has_archive'           => true,
             'exclude_from_search'   => false,
             'publicly_queryable'    => true,
             'capability_type'       => 'page',
         );
+
+        if (isset($resource['options'])) {
+            $args = array_merge($args, $resource['options']);
+        }
+
+        $args['labels'] = $labels;
 
         register_post_type( $slug, $args );
     }
