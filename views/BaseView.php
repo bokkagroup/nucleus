ed113f7d771a047b98ee8d3617b7d2474a1b2c07
Charlie Stanard <charlie@bokkagroup.com>
2018-08-27 17:33:56 -0600

    Reverting update to return filter results as variable

--- views/BaseView.php
+++ views/BaseView.php
@@ -30,7 +30,7 @@ Class View {
         }
 
         if (isset($template)) {
-            $data = apply_filters('catatlystwp_nucleus_filter_before_render', $data);
+            apply_filters('catatlystwp_nucleus_filter_before_render', $data);
 
             if (is_array($template)) {
                 if (isset($template['overview']) && $this->viewType === 'overview') {
